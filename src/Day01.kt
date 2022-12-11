8f83d7f374209f349f66f156554efbe6d48edf4d
David Knaack <davidkna@users.noreply.github.com>
2022-12-11 16:43:22 +0100

    Update Day01.kt

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,48 +1,22 @@
 fun main() {
-    fun part1(input: List<String>): Int {
-        var max = 0
-        var current = 0
-        for (line in input) {
-            if (line.isEmpty()) {
-                max = max.coerceAtLeast(current)
-                current = 0
-                continue
-            }
-            val value = Integer.parseInt(line)
-            current += value
-        }
-        return max.coerceAtLeast(current)
-    }
+    fun part1(input: List<List<String>>): Int = input.maxOf { l -> l.sumOf { n -> n.toInt() } }
 
-    // Find top 3
-    fun part2(input: List<String>): Int {
-        var max = arrayListOf(0, 0, 0)
-        var current = 0
-        for (line in input) {
-            if (line.isEmpty()) {
-                if (current > max[0]) {
-                    max[0] = current
-                    max.sort()
-                }
-
-                current = 0
-                continue
+    fun part2(input: List<List<String>>): Int {
+        val max = intArrayOf(0, 0, 0)
+        input.map { l -> l.sumOf { n -> n.toInt() } }.forEach { n ->
+            if (n > max[0]) {
+                max[0] = n
+                max.sort()
             }
-            val value = Integer.parseInt(line)
-            current += value
-        }
-        if (current > max[0]) {
-            max[0] = current
         }
         return max.sum()
     }
 
-    // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
+    val testInput = readInputDoubleNewline("Day01_test")
     check(part1(testInput) == 24000)
     check(part2(testInput) == 45000)
 
-    val input = readInput("Day01")
+    val input = readInputDoubleNewline("Day01")
     println(part1(input))
     println(part2(input))
 }
